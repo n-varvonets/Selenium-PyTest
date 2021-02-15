a16b7328ebe447d7f1f8283a5fcd8a9bd83e18df
Nickolay Varvonets <71024409+n-varvonets@users.noreply.github.com>
2021-02-16 01:12:56 +0200

    Update main.py
    try/except

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
 import time
+import sys
 
 
 class LoginIWannaBe(unittest.TestCase):
@@ -11,16 +12,43 @@ class LoginIWannaBe(unittest.TestCase):
 
     def test_01(self):
         driver = self.driver
-        button_login = driver.driver.find_element_by_link_text('/login')
+        try:
+            assert "Python" in driver.title
+        except Exception:
+            print('Err in LoginIWannaBe by test_01')
+            print(sys.exc_info())
+
+    def test_02(self):
+        driver = self.driver
+        button_login = driver.find_element_by_link_text('/login')
         button_login.send_keys(Keys.ENTER)
 
         time.sleep(2)
+
+        try:
+            assert "Python" in driver.title
+        except Exception:
+            print('Err in LoginIWannaBe by test_02')
+            print(sys.exc_info())
+
+        asddsad  = driver.find_element_by_class_name("login-img")
+
+
+
+
+        assert "Python" in driver.title
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+        driver.close()
+
 
     def tearDown(self):
         self.driver.quit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
 
 
