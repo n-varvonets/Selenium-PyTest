248ae0a4c167e5f7add3e0eb28bdda61ed08c075
Nickolay Varvonets <71024409+n-varvonets@users.noreply.github.com>
2021-03-03 18:43:47 +0200

    added complited tasks of mark.skip and parametrize

---
+++ Stepic/test_module3_less6_step3.py
@@ -0,0 +1,32 @@
+import pytest
+from selenium import webdriver
+import math
+import time
+
+def calc(x):
+  return str(math.log(int(time.time() + 20.9)))
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(executable_path='C:/Users/NIk/PycharmProjects/chromedriver.exe')
+    yield browser
+    # print("\nquit browser..")
+    # browser.quit()
+
+@pytest.mark.parametrize('links', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
+def test_guest_should_see_login_link(browser, links):
+    link = f"https://stepik.org/lesson/{links}/step/1/"
+    browser.get(link)
+    browser.implicitly_wait(5)
+    res = str(calc(time.time()))
+    browser.find_element_by_xpath("//textarea").send_keys(res)
+    browser.find_element_by_class_name("submit-submission").click()
+    # correct = browser.find_element_by_class_name("smart-hints__hint").text
+    # time.sleep(15)
+    # assert "Correct!" == correct
+
+
+# pytest -s -v test_module3_less6_step3.py
+
