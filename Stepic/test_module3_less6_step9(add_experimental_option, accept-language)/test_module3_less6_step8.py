e9db495c2e3e22012a18b51bc74c458873864553
Nickolay Varvonets <71024409+n-varvonets@users.noreply.github.com>
2021-03-06 22:46:56 +0200

    added logic es and fr

---
+++ Stepic/test_module3_less6_step9(add_experimental_option, accept-language)/test_module3_less6_step8.py
@@ -0,0 +1,5 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element_by_class_name("btn-add-to-basket")
