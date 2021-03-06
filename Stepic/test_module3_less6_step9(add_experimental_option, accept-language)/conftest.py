11619a6a9e43665a69c60acb24da738957c8c0b7
Nickolay Varvonets <71024409+n-varvonets@users.noreply.github.com>
2021-03-06 22:46:22 +0200

    add logic es and fr

--- Stepic/test_module3_less6_step9(add_experimental_option, accept-language)/conftest.py
+++ Stepic/test_module3_less6_step9(add_experimental_option, accept-language)/conftest.py
@@ -1,19 +1,23 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="chrome",
+                     help="Choose language: es or fr")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     browser = None
-    if browser_name == "chrome":
+    options = Options()
+    if browser_name == "es":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(executable_path='C:/Users/NIk/PycharmProjects/chromedriver.exe')
-    elif browser_name == "firefox":
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(executable_path='C:/Users/NIk/PycharmProjects/chromedriver.exe', options=options)
+    elif browser_name == "fr":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox(executable_path='C:/Users/NIk/PycharmProjects/geckodriver.exe')
     else:
@@ -21,5 +25,3 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-
