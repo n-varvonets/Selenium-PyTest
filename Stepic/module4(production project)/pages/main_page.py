842ee7f56dae2a5f7e451961d53afa8972c3ba27
Nickolay Varvonets <71024409+n-varvonets@users.noreply.github.com>
2021-03-11 18:33:45 +0200

    added logic  main_page.py

---
+++ Stepic/module4(production project)/pages/main_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage):
+
+    # def go_to_login_page(browser):
+    """Чтобы все работало, надо слегка видоизменить его. В аргументы больше не надо передавать экземпляр браузера,
+     мы его передаем и сохраняем на этапе создания Page Object. Вместо него нужно указать аргумент self,
+        чтобы иметь доступ к атрибутам и методам класс"""
+
+    def go_to_login_page(self):
+        # login_link = self.browser.find_element_by_css_selector("#login_link")
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+
+    """в классе MainPage нужно реализовать метод, который будет проверять наличие ссылки(login)"""
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+
