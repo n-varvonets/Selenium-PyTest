d1abc0886f4790a2e0327eb72b8b9bbfd41c7006
Nickolay Varvonets <71024409+n-varvonets@users.noreply.github.com>
2021-03-12 16:02:03 +0200

    added new method
    added new method is_element_present for checking element on page

--- Stepic/module4(production project)/pages/base_page.py
+++ Stepic/module4(production project)/pages/base_page.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import NoSuchElementException
+
 """Для начала сделаем базовую страницу, от которой будут унаследованы все остальные классы.
 В ней мы опишем вспомогательные методы для работы с драйвером.""" 
 
@@ -14,5 +16,16 @@ class BasePage():
 
     def open(self):  #  Он должен открывать нужную страницу в браузере, используя метод get()
         self.browser.get(self.url)
+
+
+    def is_element_present(self, how, what):
+        """реализуем метод, в котором будем перехватывать исключение. В него будем передавать два аргумента:
+             как искать (css, id, xpath и тд) и собственно что искать (строку-селектор).Теперь для всех проверок,
+         что элемент действительно присутствует на странице, мы можем использовать этот метод """
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
 
