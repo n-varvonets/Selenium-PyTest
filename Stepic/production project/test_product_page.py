6f0d5f0d54c9941bd8ad1afccea127e36b39d1a3
NIck <19835168>
2021-03-16 18:12:29 +0200

    new changes for today

--- Stepic/production project/test_product_page.py
+++ Stepic/production project/test_product_page.py
@@ -1,16 +1,12 @@
-from pages.product_page import AddTobasket
-import time
+from pages.product_page import AddToBasket
 
 
 def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = AddTobasket(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = AddToBasket(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.add_to_basket()             # выполняем метод страницы — переходим на страницу логина
     page.solve_quiz_and_get_code()
     page.check_methods_after_add_prod_to_cart()
-    # time.sleep(995)
-
-
 
 
