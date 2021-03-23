2dec17f406d8692bfdaad4241bdd8283fbb4a9b0
Nickolay Varvonets <71024409+n-varvonets@users.noreply.github.com>
2021-03-23 11:08:17 +0200

    Update base_page.py
    added complited test about adding product in basket from different pages

--- Stepic/production project/pages/base_page.py
+++ Stepic/production project/pages/base_page.py
@@ -2,7 +2,7 @@ from telnetlib import EC
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException  # в начале файла
-import math
+import math 
 from selenium.webdriver.support.wait import WebDriverWait
 from .locators import BasePageLocators , BasketPageLocators
 
